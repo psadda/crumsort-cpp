be4fe91fe7798f786483c6607d8699b7ade8f225
psadda <psadda@gmail.com>
2024-09-25 20:23:03 -0400

    Fix build error on GCC

--- bench/scandum_sorts.c
+++ bench/scandum_sorts.c
@@ -6,5 +6,7 @@
 #include "gridsort.h"
 #include "quadsort.h"
 
+#if !(DBL_MANT_DIG < LDBL_MANT_DIG)
 // Dummy function to allow gridsort to compile
 void quadsort_swap128(void* array, void* swap, size_t swap_size, size_t nmemb, CMPFUNC* cmp) {}
+#endif
